dd33b95dfeff2915bc31729b53bce37e0ec13ab7
Saurabh Singh Maurya <mynamesaurabh1@gmail.com>
2025-02-16 14:19:54 +0530

    ff

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
-    origin:'https://jobportal-dggi.onrender.com',
+    origin:'http://localhost:5173',
     credentials:true
 }
 app.use(cors(corsOptions));
