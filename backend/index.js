02146455cb386d925940586c2d1537c86c5b7381
Saurabh Singh Maurya <mynamesaurabh1@gmail.com>
2025-02-16 14:58:20 +0530

    origin changfes

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
-    origin:'https://jobportal-dggi.onrender.com/',
+    origin:'https://jobportal-dggi.onrender.com',
     credentials:true
 }
 app.use(cors(corsOptions));
